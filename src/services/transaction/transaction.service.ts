353dcc83fbb1d918368e8a132f375465461619af
Alibi Melis <melis.alibi2000@gmail.com>
2023-04-11 02:44:07 +0800

    Increase pagination

--- src/services/transaction/transaction.service.ts
+++ src/services/transaction/transaction.service.ts
@@ -1,28 +1,32 @@
 // Initializes the `transaction` service on path `/transaction`
-import { ServiceAddons } from '@feathersjs/feathers';
-import { Application } from '../../declarations';
-import { Transaction } from './transaction.class';
-import createModel from './transaction.model';
-import hooks from './transaction.hooks';
+import { ServiceAddons } from "@feathersjs/feathers";
+import { Application } from "../../declarations";
+import { Transaction } from "./transaction.class";
+import createModel from "./transaction.model";
+import hooks from "./transaction.hooks";
 
 // Add this service to the service type index
-declare module '../../declarations' {
+declare module "../../declarations" {
   interface ServiceTypes {
-    'transaction': Transaction & ServiceAddons<any>;
+    transaction: Transaction & ServiceAddons<any>;
   }
 }
 
 export default function (app: Application): void {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    // paginate: app.get('paginate')
+    paginate: {
+      default: 100,
+      max: 200,
+    },
   };
 
   // Initialize our service with any options it requires
-  app.use('/transaction', new Transaction(options, app));
+  app.use("/transaction", new Transaction(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('transaction');
+  const service = app.service("transaction");
 
   service.hooks(hooks);
 }
